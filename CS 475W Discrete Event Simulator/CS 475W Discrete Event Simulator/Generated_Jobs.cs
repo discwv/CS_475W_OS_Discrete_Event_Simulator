e05d82adb1cd4b540695dd9fa6fc83e0c7b68ca6
unknown <pprice16@LAB-SCI-304-17.whitworth.edu>
2015-04-08 09:56:55 -0700

    Implemented Jobs and Generated_Jobs
    Reads in from randomly generated file

--- CS 475W Discrete Event Simulator/CS 475W Discrete Event Simulator/Generated_Jobs.cs
+++ CS 475W Discrete Event Simulator/CS 475W Discrete Event Simulator/Generated_Jobs.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CS_475W_Discrete_Event_Simulator
 {
     class Generated_Jobs
     {
+        private List<Job> AllJobs;
+
+        public int NextJobStartTime
+        {
+            get { return AllJobs[0].startTime; }
+            set { }
+        }
+        public Generated_Jobs(string filename)
+        {
+            StreamReader inputFile = new StreamReader(filename);
+            string lineNumJobs = inputFile.ReadLine();
+            int[] firstLine = lineNumJobs.Split(',').Select(int.Parse).ToArray();
+            int numJobs = firstLine[0];
+
+            while(numJobs-- > 0)
+            {
+                List<string> jobStrings = new List<string>();
+                jobStrings.Add(inputFile.ReadLine());
+                int numThreads = jobStrings[0].Split(',').Select(int.Parse).ToArray()[1];
+                while (numThreads-- > 0)
+                {
+                    jobStrings.Add(inputFile.ReadLine());
+                }
+                AllJobs.Add(new Job(jobStrings.ToArray()));
+            }
+        }
+
+        public Job GetNextJob()
+        {
+            Job nextJob = AllJobs[0];
+            AllJobs.RemoveAt(0);
+            return nextJob;
+        }
     }
 }
--- CS 475W Discrete Event Simulator/CS 475W Discrete Event Simulator/Job.cs
+++ CS 475W Discrete Event Simulator/CS 475W Discrete Event Simulator/Job.cs
@@ -8,5 +8,34 @@ namespace CS_475W_Discrete_Event_Simulator
 {
     class Job
     {
+        private int myStartTime;
+        public int startTime
+        {
+            get { return myStartTime; }
+            set { myStartTime = value; }
+        }
+        private List<List<int>> myThreads;
+
+        public Job(string[] inputString)
+        {
+            int[] firstLine = inputString[0].Split(',').Select(int.Parse).ToArray();
+            myStartTime = firstLine[0];
+            for (int i = 0; i < firstLine[1]; i++)
+            {
+                List<int> line = inputString[i+1].Split(',').Select(int.Parse).ToList();
+                myThreads.Add(line);
+            }
+        }
+        public List<int> GetThread(int threadNum)
+        {
+            if (threadNum < 0 || threadNum >= myThreads.Count)
+            {
+                return null;
+            }
+            else
+            {
+                return myThreads[threadNum];
+            }
+        }
     }
 }
